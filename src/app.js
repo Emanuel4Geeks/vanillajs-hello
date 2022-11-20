1e72ce671de9ece55a8a78c8eeb9a1d922fac820
Emanuel Ciotoi <emanuel12m@gmail.com>
2022-11-20 20:53:33 +0000

    Generador sencillo finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pExcuse = document.getElementById("excuse");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateText() {
+    let whoRandom = who[getRandom(who.length)];
+    let whoCapitalized = whoRandom.charAt(0).toUpperCase() + whoRandom.slice(1);
+
+    pExcuse.innerHTML = `${whoCapitalized} ${what[getRandom(what.length)]} ${
+      when[getRandom(when.length)]
+    }`;
+  }
+
+  const getRandom = size => Math.floor(Math.random() * size);
+
+  generateText();
 };
